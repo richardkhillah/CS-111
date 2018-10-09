16dc95c5bcf1c781a1065e1262d942b8b5728144
Richard Khillah <RKhillah@ucla.edu>
2018-10-09 16:56:39 -0700

    implement basic non-canon w/ no-echo read/write functionality

---
+++ 1/lab1a.c
@@ -0,0 +1,119 @@
+// NAME: Richard Khillah
+// EMAIL: RKhillah@ucla.edu
+// UID: 604853262
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <termios.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
+struct termios saved_attributes;
+const char* program_name = NULL;
+
+void reset_input_mode(void) {
+  tcsetattr(STDIN_FILENO, TCSANOW, &saved_attributes);
+}
+
+void set_input_mode(void) {
+  struct termios tattr;
+  //  char* name;
+  
+  //TODO: Change error message
+  if(!isatty(STDIN_FILENO)) {
+    fprintf(stderr, "Not a terminal.\n");
+    exit(1);
+  }
+  
+  /* Save original attributes so they can be restored later */
+  tcgetattr(STDIN_FILENO, &saved_attributes);
+  atexit(reset_input_mode);
+
+  /* Set new terminal mode */
+  tcgetattr(STDIN_FILENO, &tattr);
+  tattr.c_iflag = ISTRIP;   /* use only lower 7 bits */
+  tattr.c_oflag = 0;        /* no processing */
+  tattr.c_lflag = 0;        /* no processing */
+  
+  tcsetattr(STDIN_FILENO, TCSANOW, &tattr);
+}
+
+void set_program_name(const char* argv0) {
+  if(argv0 == NULL) {
+    fprintf(stderr, "%s: set_program_name: %s.\n",	\
+	    program_name, strerror(errno));
+    abort();
+  }
+
+  const char* slash = strrchr(argv0, '/');
+  const char* base = (slash != NULL ? slash + 1 : argv0);
+
+  argv0 = base;
+
+  program_name = argv0;
+}
+
+//TODO: Move all constants and globals to top of page
+const unsigned short RBUF_SIZE = 256;
+const unsigned short WBUF_SIZE = 512; /* Assume all reads are <cr> or <lf>*/
+
+void rw_input(void) {
+  char* rbuf[RBUF_SIZE];
+  char* wbuf[WBUF_SIZE];
+  
+  while(1) {
+    int rb_size = read(STDIN_FILENO, rbuf, RBUF_SIZE);
+    if (rb_size < 0) {
+      fprintf(stderr, "%s: %s\n", program_name, strerror(errno));
+      exit(1);
+    }
+
+    /* process only the amount of bytes that were read into rbuf */
+    int rb_i;
+    int wb_size = 0;   /* wbuf might be larger than rbuf after mapping */
+    for(rb_i = 0; rb_i < rb_size; rb_i++){
+      char c = rbuf[rb_i];
+      switch (c) {
+      case 0x04: // ^D
+	//fprintf(stderr, "Exiting\n");
+	exit(0);
+	break;     // this is unnecessary but we'll put it in anyway.
+      case '\r':
+      case '\n':
+	wbuf[wb_size++] = '\r';
+	wbuf[wb_size++] = '\n';
+	//case 0x0a:
+	//case 0x0d:
+	break;
+      default:
+	wbuf[wb_size++] = c; // fixthis
+	break;
+      }
+    }
+    
+    /* write buffer to screen */
+    /* int bytes_written = write(STDOUT_FILENO, wbuf, wb_size+1); */
+    int bytes_written;
+    for(bytes_written = 0; bytes_written < wb_size; bytes_written++) {
+      write(STDOUT_FILENO, &wbuf[bytes_written], sizeof(char));
+    }
+    
+    if (bytes_written < 0) {
+      fprintf(stderr, "%s: %s\n", program_name, strerror(errno));
+      exit(1);
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  set_input_mode();
+  set_program_name(argv[0]);
+  
+  while(1) {
+    rw_input();
+  }
+  return 0;
+}

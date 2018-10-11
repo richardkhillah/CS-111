2715d961711255772b0224667630d52df487052c
Richard Khillah <RKhillah@ucla.edu>
2018-10-10 21:56:58 -0700

    somewhere with things

--- 1A/lab1a.c
+++ 1A/lab1a.c
@@ -13,6 +13,8 @@
 #include <getopt.h>
 #include <signal.h>
 #include <poll.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 struct termios saved_attributes;
 const char* program_name = NULL;
@@ -35,9 +37,10 @@ int rc_flag = 0;
 pid_t rc;            /* child process */
 
 void Error(void);
+void print_shell_exit_status();
 void run_shell(int pipe2shell[2], int pipe2term[2]);
-void process_shell_output(int read_fd);
-int process_keyboard_input(int pipe2shell, int pipe2term);
+int process_shell_output(int read_fd);
+int process_keyboard_input(int pipe2shell);
 void init_pipe(int pipe[]);
 void reset_input_mode(void);
 void set_input_mode(void);
@@ -45,7 +48,7 @@ void set_program_name(const char*);
 void set_options(int argv, char* args[]);
 void rw_input(void);
 
-void process_shell_output(int read_fd) {
+int process_shell_output(int read_fd) {
   char rbuf[RBUF_SIZE];
   int rb_size = read(read_fd, rbuf, RBUF_SIZE);
   if(rb_size < 0)
@@ -55,11 +58,12 @@ void process_shell_output(int read_fd) {
   for(rb_i = 0; rb_i < rb_size; rb_i++) {
     char c = rbuf[rb_i];
       switch (c) {
-      case 0x04: // ^D == EOF	
+      case 0x04: // ^D == EOF
+	return 0x04;
 	exit(0);
       case '\r':
       case '\n':
-	if(write(STDOUT_FILENO, &LF, sizeof(char)*2) < 0)
+	if(write(STDOUT_FILENO, &LF, sizeof(char)) < 0)
 	  Error();
 	break;
       default:
@@ -68,9 +72,10 @@ void process_shell_output(int read_fd) {
 	break;
       }
   }
+  return 0;
 }
 
-int process_keyboard_input(int pipe2shell, int pipe2term) {
+int process_keyboard_input(int pipe2shell) {
   /* read from stdin, write to stdout and forward (write) to shell */
   char rbuf[RBUF_SIZE];
 
@@ -84,12 +89,13 @@ int process_keyboard_input(int pipe2shell, int pipe2term) {
     for(rb_i = 0; rb_i < rb_size; rb_i++) {
       char c = rbuf[rb_i];
       switch(c) {
-      case 0x03:// ^C : interrupt character
-	kill(rc, SIGINT);
-	//	exit(1);
-      case 0x04: // ^D : EOF
+      case 0x03:          /* ^C : interrupt character */
+	kill(rc, SIGTERM);
+	return 0x03;
+	// what do i do from here??
+      case 0x04:          /* ^D : EOF */
 	close(pipe2shell);
-	return 0x04; // no more keyboard input
+	return 0x04;
       case '\r':
       case '\n':
 	if(write(STDOUT_FILENO, &CRLF, sizeof(char)*2) < 0)
@@ -131,10 +137,10 @@ int main(int argc, char* argv[]) {
 
     /* Setup terminal polling service */
     struct pollfd pollfds[2];
-    pollfds[0].fd = 0;
+    pollfds[0].fd = 0;              /* stdin */
     pollfds[0].events = POLLIN;
     pollfds[0].revents = 0;
-    pollfds[1].fd = pipe2term[0];
+    pollfds[1].fd = pipe2term[0];   /* input from shell */
     pollfds[1].events = POLLIN | POLLHUP | POLLERR;
     pollfds[1].revents = 0;
         
@@ -146,34 +152,35 @@ int main(int argc, char* argv[]) {
 
       /* block shell input and read input from keyboard */
       if(pollfds[0].revents & POLLIN) {
-    	/* handle input from keyboard */
 	int ret = process_keyboard_input(pipe2shell[1]);
-	switch(ret) {
-	case 0x03:// ^C : interrupt character
-	  continue;
-	case 0x04: // ^D : EOF
-	  // close(pipe2shell);
-	default:
+	if (ret == 0x03)  /* ^C : interrupt character */
 	  break;
-	
-	}
+	if (ret == 0x04)  /* ^D : EOF */
+	  /* process_keyboard_input already closed pipe2shell */
+	  continue;
       }
 
       if(pollfds[1].revents & POLLIN) {
-    	/* handle input from shell */
-	process_shell_output(pipe2term[0]);
-      }
+	int ret = process_shell_output(pipe2term[0]);
+	if (ret == 0x04) {
+	  close(pipe2term[0]);
+	  break;
+	}
 
-      if(pollfds[1].revents & (POLLHUP + POLLERR)) {
-    	/* kill shell */
-      }
+	if(pollfds[1].revents & (POLLHUP + POLLERR)) {
+	  /* kill shell */
+	  kill(rc, SIGINT);
+	  break;
+	}
+      }  
     } // end while
   } else {
     while(1) {
       rw_input();
     }
   }
-  
+
+  print_shell_exit_status();
   return 0;
 } // end main
 
@@ -299,5 +306,15 @@ void run_shell(int pipe2shell[2], int pipe2term[2]) {
     char* const args[] = {NULL};
     if(execvp(file, args) < 0)
       Error();
+  }
+}
+
+void print_shell_exit_status(){
+  if(rc_flag){
+    int status;
+    if(waitpid(-1, &status, 0) == -1)
+      Error();
+    fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d",
+	    WTERMSIG(status), WEXITSTATUS(status));
   }
 }

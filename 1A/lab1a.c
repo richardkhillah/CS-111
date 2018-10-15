5ceb6fd7505f517c9e70a43eb8f944ae19ce7782
WhiteRabbit27 <RKhillah@ucla.edu>
2018-10-14 23:36:00 -0700

    Removed and Moved things aroung
    discarded old 1A, moved archive/1A to main Labs directory and
    removed test directory.

--- 1A/lab1a.c
+++ 1A/lab1a.c
@@ -18,228 +18,224 @@
 
 struct termios saved_attributes;
 const char* program_name = NULL;
-const int TIMEOUT = 0;
-const unsigned short BUF_SIZE = 256;
-//const unsigned short WBUF_SIZE = 512;
-char crlf[2] = {'\r', '\n'};
-char lf[1] = {'\n'};
 
+static struct option const long_opts[] = {
+    {"shell", no_argument, NULL, 's'},
+    {"debug", no_argument, NULL, 'd'},
+    {NULL, 0, NULL, 0}
+};
+
+const int TIMEOUT = 0;
+const unsigned short RBUF_SIZE = 256;
+const unsigned short WBUF_SIZE = 512;
 const char CR = '\r';
 const char LF = '\n';
 const char CRLF[] = {'\r', '\n'};
 
-int shell = 0;
-int dflag = 0;
-//char* shell_program;
-pid_t shellpid;            /* child process */
+// global for now...
+//TODO: localize these guys.
+int rc_flag = 0; // shell flag
+int d_flag = 0;  // debug flag
 
-int pipe2shell[2];
-int pipe2term[2];
+pid_t rc;            /* child process */
 
+// global pipes for now
+/* int pipe2shell[2]; */
+/* int pipe2term[2]; */
+
+void sighanler(int signum);
+void debug(char* msg);
 
 void Error(void);
-void debug(char* func, char* message);
-
-void sig_handler(int signum);
+void print_shell_exit_status();
+void run_shell(int pipe2shell[2], int pipe2term[2]);
+int process_shell_output(int read_fd);
+int process_keyboard_input(int pipe2shell);
+void init_pipe(int pipe[]);
 void reset_input_mode(void);
 void set_input_mode(void);
 void set_program_name(const char*);
-void set_options(int argc, char* args[]);
+void set_options(int argv, char* args[]);
 void rw_input(void);
 
-void init_pipes();
-void run_shell();
-void print_shell_exit_status(int status);
+int process_shell_output(int read_fd) {
+    char rbuf[RBUF_SIZE];
+    int rb_size = read(read_fd, rbuf, RBUF_SIZE);
+    if(rb_size < 0)
+	Error();
 
-void run_terminal(void);
-void read_keyboard(void);
-void read_shell(void);
+    int rb_i;
+    for(rb_i = 0; rb_i < rb_size; rb_i++) {
+	char c = rbuf[rb_i];
+	switch (c) {
+	case 0x04: // ^D == EOF
+	    return 0x04;
+	case '\n':
+	    if(write(STDOUT_FILENO, "\r\n", sizeof(char)*2) < 0)
+		Error();
+	    break;
+	default:
+	    if(write(STDOUT_FILENO, &c, sizeof(char)) < 0)
+		Error();
+	    break;
+	}
+    }
+    return 0;
+}
 
-int read_shell_output(int read_fd);
-int read_keyboard_input(int pipe2shell);
+int process_keyboard_input(int pipe2shell) {
+    /* read from stdin, write to stdout and forward (write) to shell */
+    char rbuf[RBUF_SIZE];
+
+    int rb_size;
+    while(1) {
+	rb_size = read(STDIN_FILENO, rbuf, sizeof(char)*RBUF_SIZE);
+	if(rb_size < 0)
+	    Error();
+
+	int rb_i;
+	for(rb_i = 0; rb_i < rb_size; rb_i++) {
+	    char c = rbuf[rb_i];
+	    switch(c) {
+	    case 0x03:          /* ^C : interrupt character */
+		if(d_flag) debug("^C");
+		return 0x03;
+	    case 0x04:          /* ^D : EOF */
+		if(d_flag) debug("^D");
+		return 0x04;
+	    case '\r':
+		if(d_flag) debug("\\r");
+	    case '\n':
+		if(d_flag) debug("\\n");
+		if(write(STDOUT_FILENO, "\r\n", sizeof(char)*2) < 0)
+		    Error();
+		if(write(pipe2shell, "\n", sizeof(char)) < 0)
+		    Error();
+		break;
+	    default:
+		if(write(STDOUT_FILENO, &c, sizeof(char)) < 0)
+		    Error();
+		if(write(pipe2shell, &c, sizeof(char)) < 0)
+		    Error();
+		break;
+	    }
+	}
+    }
+}
+
+void sighandler(int signum) {
+    if(signum == SIGPIPE) {
+	// trying to write to closed pipe.
+	//print_shell_exit_status();
+	exit(0);
+    }
+
+    if(signum == SIGINT) {
+	if(d_flag) debug("SIGINT Received");
+	//print_shell_exit_status();
+	exit(1);
+    }
+}
 
 int main(int argc, char* argv[]) {
     set_input_mode();
     set_program_name(argv[0]);
     set_options(argc, argv);
 
-    if(shell) {
-	init_pipes();
-	shellpid = fork();
-    
-	if(shellpid < 0) Error();
+    if(rc_flag == 1) {
+	int pipe2shell[2];
+	int pipe2term[2];
+	init_pipe(pipe2shell);
+	init_pipe(pipe2term);
 
-	if(shellpid == 0) 
-	    run_shell();
-	else
-	    run_terminal();
+	/* fork rc the run shell */
+	int child = fork();
+	if(child < 0) Error();
+
+	else if(child == 0) {
+	    run_shell(pipe2shell, pipe2term);
+	}
+
+	else {
+	    /* Widow unused ends of pipes */
+	    close(pipe2shell[0]);
+	    close(pipe2term[1]);
+
+	    /* Setup terminal polling service */
+	    struct pollfd pollfds[2];
+	    pollfds[0].fd = 0;              /* stdin */
+	    pollfds[0].events = POLLIN;
+	    pollfds[0].revents = 0;
+	    pollfds[1].fd = pipe2term[0];   /* input from shell */
+	    pollfds[1].events = POLLIN | POLLHUP | POLLERR;
+	    pollfds[1].revents = 0;
+
+	    //	    int child_status;
+	    /* run main loop */
+	    while (1) {
+		/* Poll terminal inputs */
+		int poll_result = poll(pollfds, 2, TIMEOUT);
+		if( poll_result  == -1)
+		    Error();
+		if( poll_result == 0)
+		    continue;
+      
+		/* block shell input and read input from keyboard */
+		if(pollfds[0].revents & POLLIN) {
+		    int ret = process_keyboard_input(pipe2shell[1]);
+		    if(ret < 0)
+			Error();
+		
+		    /* ^C : interrupt character */
+		    if (ret == 0x03) {
+			kill(child, SIGINT);
+			//waitpid(child, &child_status, 0);
+			print_shell_exit_status(child);
+			exit(0);
+		    }
+	
+		    /* ^D : EOF */
+		    if (ret == 0x04) {
+			close(pipe2shell[1]);
+		    }
+		} // end keyboard read
+      
+		/* block keyboard input and read output from shell */
+		if(pollfds[1].revents & POLLIN) {
+		    if(d_flag) debug("reading shell input");
+		    int ret = process_shell_output(pipe2term[0]);
+		    if(ret < 0)
+			Error();
+		} // end shell read
+
+		/* Something happend, so process remaining work then exit */
+		if(pollfds[1].revents & (POLLHUP | POLLERR)) {
+		    /* kill shell */
+		    if(d_flag) debug("POLLHUP | POLLERR received");
+		    print_shell_exit_status(child);
+		    //exit(1);
+		    break;
+		} // end shell error or hup
+	    } // end while
+	    //	    print_shell_exit_status();
+	}
     } else {
-	rw_input();
+	while(1) {
+	    rw_input();
+	}
     }
-  
-    int status;
-    if(waitpid(shellpid, &status, 0) < 0)
-	Error();
-    print_shell_exit_status(status);
+
     return 0;
-}
+} // end main
 
-void sig_handler(int signum){
-    if(shell && signum == SIGINT)
-	kill(shellpid, SIGINT);
-    if(signum == SIGPIPE)
-	exit(1);
-}
-
-void print_shell_exit_status(int status) {
-    fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d",
-	    WTERMSIG(status), WEXITSTATUS(status));
-
-}
-
-void read_shell(void) {
-    char buf[BUF_SIZE];
-    while(1) {
-	//    int bytes_read = read(pipe2term[0], buf, BUF_SIZE);
-	int bytes_read = read(pipe2term[0], buf, BUF_SIZE);
-	if (bytes_read < 0) Error();
-
-	int i;
-	for(i = 0; i < bytes_read; i++) {
-	    char c= buf[i];
-	    switch(c) {
-	    case 0x04: // ^D
-		if(dflag) debug("^D","read_shell");
-		close(pipe2term[1]); // close shell's write pipe ie shell output
-		//exit(0);
-		return;
-	    case '\r':
-	    case '\n':
-		//if(dflag) debug("case: \'\\n\'", "read_shell");
-		//write(STDOUT_FILENO, crlf, 2);
-		write(STDOUT_FILENO, &"\r", 1);
-		write(STDOUT_FILENO, &"\n", 1);
-		break;
-	    default:
-		//if(dflag) debug(&c, "read_shell case default");
-		write(STDOUT_FILENO, &c, 1);
-		break;
-	    } // end switch 
-	}//end for
-    }//end while
-}
-
-int enable_keyboard = 1;
-void read_keyboard(void) {
-    char buf[BUF_SIZE];
-    int status = 0;
-    while(1) {
-	int bytes_read = read(STDIN_FILENO, buf, BUF_SIZE);
-	if(bytes_read < 0) Error();
-
-	int i;
-	for(i = 0; i < bytes_read; i++) {
-	    char c = buf[i];
-	    switch (c){
-	    case 0x04: /* ^D */
-		if(dflag) debug("read_keyboard", "^D");
-		close(pipe2shell[1]);  // close term write pipe ie shell input
-		waitpid(shellpid,&status,0);	// continue to process shell output
-		print_shell_exit_status(status);
-		exit(0);
-		continue;
-	    case 0x03: /* ^C */
-		if(dflag) debug( "^C", "read_keyboard");
-		kill(shellpid, SIGINT);
-		/*check if this is correct*/
-		close(pipe2term[0]);
-		waitpid(shellpid, &status, 0);
-		print_shell_exit_status(SIGINT);
-		//	exit(0);
-		break;
-	    case '\r':
-	    case '\n':
-		write(STDOUT_FILENO, crlf, 2);
-		write(pipe2shell[1], lf, 1);
-		break;
-	    default:
-		write(STDOUT_FILENO, &c, 1);	
-		write(pipe2shell[1], &c, 1);
-		break;
-	    }// end switch
-	} // end for
-    } // end while
-}
-
-void run_terminal(void) {
-    close(pipe2shell[0]); // because parent writes to shell
-    close(pipe2term[1]); // beacuse parent reads from shell
-
-    struct pollfd pfds[2];
-    pfds[0].fd = STDIN_FILENO; // input from keyboard
-    pfds[1].fd = pipe2term[0]; // input from shell
-
-    pfds[0].events = 0;
-    pfds[1].events = 0;
-    pfds[0].events = POLLIN | POLLHUP | POLLERR;
-    pfds[1].events = POLLIN | POLLHUP | POLLERR;
-
-    pfds[0].revents = 0; // initial vals
-    pfds[1].revents = 0; // set after each polling cycle.
-
-    while(1) {
-	if(poll(pfds, 2, TIMEOUT) == -1) Error();
-
-	if(pfds[0].revents & POLLIN) {
-	    read_keyboard(); // only, all i/o blocked
-	    //	pfds[0].revents = 0;
-	}
-	if(pfds[1].revents & POLLIN) {
-	    read_shell(); // ditto read_keyboard()
-	    //	pfds[1].revents = 0;
-	}
-	if(pfds[1].revents & (POLLERR | POLLHUP)) { // poll error from shell => SIGINT
-	    if(dflag) debug("pfds[1].revents = POLLERR | POLLHUP", "run_terminal");
-	    if(dflag) debug("read_shell() //do final read shell", "run_terminal");
-	    read_shell(); // do final read
-	    if(dflag) debug("close(pipe2term[0])", "run_terminal");
-	    close(pipe2term[0]); // all shell output has been processed by this time
-	    break;
-	}
-    }
-}
-
-void run_shell(void) {
-    close(pipe2shell[1]); // becuase child reads from this pipe
-    close(pipe2term[0]); // because child writes to this pipe
-
-    dup2(pipe2shell[0], 0); // make childs stdin
-    dup2(pipe2term[1], 1); // make childs stdout
-    dup2(pipe2term[1], 2); // make childs stderr
-
-    close(pipe2shell[0]);
-    close(pipe2term[1]);
-
-    char* args[] = {NULL};
-    //char** args = NULL;
-    //  if(execl(path, path, (char*) NULL) == -1)
-    //  if(dflag) debug("execvp()", "run_shell");
-    if(execvp("/bin/bash", args) < 0)
-	Error();    
-}
-
-void init_pipes(void) {
-    if(pipe(pipe2shell) < 0) Error();
-    if(pipe(pipe2term) < 0) Error();
+void debug(char* msg){
+    fprintf(stderr, "[debug]: %s\r\n", msg);
+    
 }
 
 void Error(void) {
-    fprintf(stderr, "%s: %s\n", program_name, strerror(errno));
+    fprintf(stderr, "%s: %s\r\n", program_name, strerror(errno));
     exit(1);
-}
-
-void debug(char* func, char* message){
-    fprintf(stderr, "[debug: %s] %s: ..in '%s'\n", program_name, message, func);
 }
 
 void set_program_name(const char* argv0) {
@@ -258,23 +254,17 @@ void set_program_name(const char* argv0) {
 }
 
 void set_options(int argc, char* argv[]){
-    static struct option const long_opts[] = {
-	{"shell", optional_argument, NULL, 's'},
-	{"debug", no_argument, NULL, 'd'},
-	{NULL, 0, NULL, 0}
-    };
-  
     int opt;
     int optind;
     while((opt = getopt_long(argc, (char* const*)argv, "sd", long_opts, &optind)) != -1) {
 	switch (opt) {
 	case 's':
-	    signal(SIGINT, sig_handler);
-	    signal(SIGPIPE, sig_handler);
-	    shell = 1;
+	    signal(SIGINT, sighandler);
+	    signal(SIGPIPE, sighandler);
+	    rc_flag = 1;
 	    break;
 	case 'd':
-	    dflag = 1;
+	    d_flag = 1;
 	    break;
 	default:
 	    Error();
@@ -283,18 +273,19 @@ void set_options(int argc, char* argv[]){
 }
 
 void rw_input(void) {
-    char buf[BUF_SIZE];
+    char rbuf[RBUF_SIZE];
 
     while(1) {
-	int bytes_read = read(STDIN_FILENO, buf, BUF_SIZE);
-	if (bytes_read < 0)
+	int rb_size = read(STDIN_FILENO, rbuf, RBUF_SIZE);
+	if (rb_size < 0)
 	    Error();
 
-	int i;
-	for(i = 0; i < bytes_read; i++){
-	    char c = buf[i];
+	/* process only the amount of bytes that were read into rbuf */
+	int rb_i;
+	for(rb_i = 0; rb_i < rb_size; rb_i++){
+	    char c = rbuf[rb_i];
 	    switch (c) {
-	    case 0x04:
+	    case 0x04: // ^D
 		exit(0);
 	    case '\r':
 	    case '\n':
@@ -335,4 +326,40 @@ void set_input_mode(void) {
     tattr.c_lflag = 0;        /* no processing */
   
     tcsetattr(STDIN_FILENO, TCSANOW, &tattr);
+}
+
+void init_pipe(int pipefd[2]){
+    if(pipe(pipefd) < 0)
+	Error();
+}
+
+void run_shell(int pipe2shell[2], int pipe2term[2]) {
+	/* setup shell i/o envrionment before calling execvp */
+	/* widow unused pipe ends */
+	close(pipe2shell[1]);  /* inputs write end */
+	close(pipe2term[0]); /* outputs read end */
+
+	/* redirect rc's stdin, stdout, stderr */
+	dup2(pipe2shell[0], 0);
+	dup2(pipe2term[1], 1);
+	dup2(pipe2term[1], 2);
+
+	/* widow redundant streams */
+	close(pipe2shell[0]);
+	close(pipe2term[1]);
+
+	const char* file = "/bin/bash";
+	if(execl(file, "bash", (char*)NULL) < 0)
+	    Error();
+}
+
+void print_shell_exit_status(int pid){
+    if(rc_flag){
+	int status;
+	//	if(waitpid(-1, &status, 0) == -1)
+	if(waitpid(pid, &status, 0) == -1)
+	    Error();
+	fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d",
+		WTERMSIG(status), WEXITSTATUS(status));
+    }
 }

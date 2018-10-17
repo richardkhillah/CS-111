cf09d17877e6af4e8307223c25aa29fdedbc1f5f
Richard Khillah <RKhillah@ucla.edu>
2018-10-17 15:54:39 -0700

    client and server comiple with one "arg not used" warning

--- 1B/lab1b-client.c
+++ 1B/lab1b-client.c
@@ -10,9 +10,15 @@
 #include <errno.h>
 #include <getopt.h>
 #include <termios.h>
+#include <poll.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
 struct termios saved_attributes;
-
+const char* program_name;
 static struct option const long_opts[] = {
     {"port", required_argument, NULL, 'p'},
     {"log", optional_argument, NULL, 'l'},
@@ -32,9 +38,12 @@ char* logfile;
 char* encryptionkey;
 char* host;
 
-int lffd;                // logfile fd
-
 int BUF_SIZE = 256;
+int TIMEOUT = 0;
+
+void err(char* msg) {
+    fprintf(stderr, "%s\n", msg);
+}
 
 void handle_error(char* msg) {
     fprintf(stderr, "[%s]: %s (errno %d)\n", msg, strerror(errno), errno);
@@ -50,46 +59,71 @@ void debug(char* msg) {
     fprintf(stderr, msg);
 }
 
-/* read_write will read from either stdin, or a network socket
- * sel(ect) detmines whether we are reading from the keyboard
- * and writing to the server (select 0) or reading from the
- * server and writing to the terminal (select 1).
- * sel = 0 : client -> server
- * sel = 1 : server -> client */
-void read_write(int readfd, int writefd, int sel) {
+void readstd(int sockfd) {
+    // read standard input
     char buf[BUF_SIZE];
-    int bytes_read = read(readfd, buf, BUF_SIZE);
-    if(bytes_read < 0) handle_error("Error reading readfd");
-
-    int i = 0;
-    for(; i < bytes_read; i++){
-	char c = buf[i];
-	if(log_flag) {
-	    FILE* wstream = fopen(logfile, "a");
-	    int i = 0;
-	    switch(sel){
-	    case 0:
-		fprintf(wstream, "SENT %d bytes: %s", bytes_read, &c);
-		break;
-	    case 1:
-		fprintf(wstream, "RECEIVED %d bytes: %s", bytes_read, &c);
-		break;
-	    default:
-		handle_error("Invalid read_write select.");
-		exit(1);
-	    }
-	}
+    FILE* logstream;
+    int bytes = read(STDIN_FILENO, buf, BUF_SIZE);
+    if( bytes < 0 ) err("unable to read standard input");
     
-	// process char by char and echo to stdout
-	if (encrypt_flag == 1 && sel == 1) {
-	    // decrypt input from server
+    if(log_flag) {
+	logstream = fopen(logfile, "a");
+	if( logstream == NULL ) err("Unable to open logfile");
+    }
 
-	    // output to stdoud
+    /* Write read buffer byte-by-byte to appropiate spots */
+    int i = 0;
+    for(; i < bytes; i++) {
+	char c = buf[i];
+	write(STDOUT_FILENO, &buf, 1);  // write( disp )
+	
+	if(encrypt_flag) {              // if incrypting
+	    // encrypt
 	}
-	write(STDOUT_FILENO, &c, sizeof(char));
-       	// write to write_fd
+	
+	write(sockfd, &c, 1);           // write( sock )
+	if(log_flag) {                  // if logging, write( log )
+	    fwrite(&c, sizeof(char), 1, logstream);
+	}
+    } // end loop
+    if(logstream) {
+	fclose(logstream);
     }
 }
+
+void readsoc(int sockfd){
+    /* read socket */
+    char buf[BUF_SIZE];
+    FILE* logstream;
+    int bytes = read(sockfd, buf, BUF_SIZE); //or BUF_SIZE-1?
+    if(bytes < 0) err("Bad soc read");
+
+    if( log_flag ) {
+	logstream = fopen(logfile, "a");
+	if(logstream == NULL) err("Failed open of logfile");
+    }
+
+    // main function loop
+    int i = 0;
+    for(; i < bytes; i++) {
+	char c = buf[i];
+	// if logging( log )
+	if( log_flag ){
+	    fwrite(&c, sizeof(char), 1, logstream);
+	}
+	if( encrypt_flag ) {
+	    // decrypt buffer
+	}
+	// write(disp)
+	write(STDOUT_FILENO, &c, sizeof(char));    
+    }// end loop
+
+    if( log_flag ) {
+	fclose(logstream);
+    }
+}
+
+
 
 void get_options(int argc, char* argv[]){
     int opt;
@@ -116,6 +150,19 @@ void get_options(int argc, char* argv[]){
 		break;
 	    }
 	}
+}
+
+void set_program_name(const char* argv0) {
+    if(argv0 == NULL) {
+	fprintf(stderr, "%s: set_program_name: %s.\n",	\
+		program_name, strerror(errno));
+	abort();
+    }
+    
+    const char* slash = strrchr(argv0, '/');
+    const char* base = (slash != NULL ? slash + 1 : argv0);
+    argv0 = base;
+    program_name = argv0;
 }
 
 void reset_input_mode(void) {
@@ -147,23 +194,67 @@ void set_input_mode(void) {
 
 int main(int argc, char* argv[]) {
     set_input_mode();
-    // set_program_name(argv[0]);
+    set_program_name(argv[0]);
     get_options(argc, argv);
 
-    // set up socket
+    /* Ensure --port=port option passed in */
+    if(!port_flag) {
+	fprintf(stderr, "Usage: --port=port\n");
+    } else if( port < 1025 ) {
+	fprintf(stderr, "Error: port must be greater than 1024\n");
+    }
+	
+    /* set up socket */
+    int sockfd;
 
-    // set up polling service
-    // listen at stdin and server socket (once connection established)
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    if(port < 1025) fprintf(stderr, "port must be greater than 1024");
+
+    /* generate a socket*/
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd < 0) err("ERROR opening socket");
     
-    // read input from keyboard and echo to display
-    // and forward to socket
+    /* get the host */
+    server = gethostbyname("localhost");
+    if (server == NULL) err("ERROR, no such host");
 
-    // if log_flag, write to log file
-    // if encrypt_flag, encrypt then forward to socket and logfile
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_UNIX;
+    bcopy((char *)server->h_addr, 
+	  (char *)&serv_addr.sin_addr.s_addr,
+	  server->h_length);
+    serv_addr.sin_port = htons(port);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+	err("ERROR connecting");
 
-    // read input from socket and write to display
-    // if log_flag, write to log file
-    // if encrypt_flag, decrypt then write to display and log file.
-    
-    return 0;
+    /* Begin polling service */
+    struct pollfd pollfds[2];
+    pollfds[0].fd = STDIN_FILENO;                   // Poll(2) stdin
+    pollfds[0].events = POLLIN;                     
+    pollfds[0].revents = 0;
+    pollfds[1].fd = sockfd;                         // Poll(2) socket
+    pollfds[1].events = POLLIN | POLLHUP | POLLERR;
+    pollfds[1].revents = 0;
+
+    while(1) {
+	/* Poll(2) fd's */
+	int poll_result = poll(pollfds, 2, TIMEOUT);
+	if( poll_result  == -1) err("Error: Bad polling.");
+	if( poll_result == 0) continue;
+      
+	/* block socket input and read input from keyboard */
+	if(pollfds[0].revents & POLLIN) readstd(sockfd);
+      
+	/* block keyboard input and read output from socket */
+	if(pollfds[1].revents & POLLIN) readsoc(sockfd);
+
+	/* Something happend, so process remaining work then exit */
+	if(pollfds[1].revents & (POLLHUP | POLLERR)) {
+	    
+	    if(debug_flag) debug("POLLHUP | POLLERR received");
+	}
+	break;
+    }
+    exit(0);
 }
--- 1B/lab1b-server.c
+++ 1B/lab1b-server.c
@@ -2,6 +2,183 @@
 // EMAIL: RKhillah@ucla.edu
 // UID: 604853262
 
-int main(void) {
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <errno.h>
+#include <getopt.h>
+#include <termios.h>
+#include <poll.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+const char* program_name;
+static struct option const long_opts[] = {
+    {"port", required_argument, NULL, 'p'},
+    {"encrypt", optional_argument, NULL, 'e'},
+    {"debug", optional_argument, NULL, 'd'},
+    {NULL, 0, NULL, 0}
+};
+
+int childpid;
+int pipe2child[2];
+int pipe2parent[2];
+
+int port_flag = 0;
+int encrypt_flag = 0;
+int debug_flag = 0;
+int host_flag = 0;
+
+int port;
+char* encryptionkey;
+char* host;
+
+int BUF_SIZE = 256;
+int TIMEOUT = 0;
+
+void err(char* msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+void handle_error(char* msg) {
+    fprintf(stderr, "[%s]: %s (errno %d)\n", msg, strerror(errno), errno);
+    exit(1);
+}
+
+void handle_failed_syscall(int signum) {
+    // print message
+    // sigpipe handling
+    exit(1);
+}
+
+void debug(char* msg) {
+    fprintf(stderr, msg);
+}
+
+void get_options(int argc, char* argv[]){
+    int opt;
+    int optind;
+    while((opt = getopt_long(argc, (char* const*)argv, \
+			     "sd", long_opts, &optind)) != -1)
+	{
+	    switch (opt) {
+	    case 'p':
+		port = atoi(optarg);
+		break;
+	    case 'e':
+		encrypt_flag = 1;
+		break;
+	    case 'd':
+		debug_flag = 1;
+		break;
+	    default:
+		// print custom error
+		break;
+	    }
+	}
+}
+
+void set_program_name(const char* argv0) {
+    if(argv0 == NULL) {
+	fprintf(stderr, "%s: set_program_name: %s.\n",	\
+		program_name, strerror(errno));
+	abort();
+    }
+    
+    const char* slash = strrchr(argv0, '/');
+    const char* base = (slash != NULL ? slash + 1 : argv0);
+    argv0 = base;
+    program_name = argv0;
+}
+
+void readsoc() {
+    
+}
+
+void readpipe() {
+
+}
+
+void shell_proc(int readpipe[2], int writepipe[2]) {
+    if(childpid == 0){
+	close(readpipe[1]);
+	close(writepipe[0]);
+
+	dup2(readpipe[0], STDIN_FILENO);
+	dup2(writepipe[1], STDOUT_FILENO);
+	dup2(writepipe[1], STDERR_FILENO);
+
+	close(readpipe[0]);
+	close(writepipe[1]);
+
+	if(execl("/bin/bash", "bash", (char*) NULL) < 0)
+	    handle_error("unable to execl");
+    }
+}
+
+void init_pipes() {
+    if(pipe(pipe2child) < 0) 
+	handle_error("Error initializing pipe2child");
+    if(pipe(pipe2parent) < 0)
+	handle_error("Error initializing pipe2parent");
+}
+
+int main(int argc, char* argv[]) {
+    set_program_name(argv[0]);
+    get_options(argc, argv);
+    
+    // setup signal handlers
+        
+    int sockfd, newsockfd, clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd < 0) handle_error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_UNIX;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+	     sizeof(serv_addr)) < 0) err("ERROR on binding");
+
+    // listen on socket     
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+
+    // accept connection
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
+    if (newsockfd < 0) err("ERROR on accept");
+
+    // setup pipes globally
+    init_pipes();
+    // fork child, redirect stdin/out/err then call exec
+    close(pipe2child[0]);
+    close(pipe2parent[1]);
+
+    // breakdown undeeded pipes in parent
+
+    // setup polling
+    // poll socket and pipes
+
+    // forward data receive from socket through pipe to shell
+    // perform mappings from socket to shell
+    //   ^C -> SIGINT
+    //   ^D -> close write side of pipe to shell
+    
+    // forward data received from pipe from shell to socket
+    //   EOF || SIGPIPE from shell
+    //        ->harvest shell complettion status, log to stderr, exit(0)
+    //   EOF || read err from network cxn (client exits), close write pipe to shell
+    //        - wait fro EOR from pipe from shell, then harvest and report term stats
+
+    // server encounters unrecognized argument, print informative usage message on stderr
+    //     exit(1)
+    
     return 0;
 }

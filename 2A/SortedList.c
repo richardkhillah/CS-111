0fab876fcda3e8bd55d00bf73acc46f7fe881377
WhiteRabbit27 <RKhillah@ucla.edu>
2018-10-23 23:55:30 -0700

    Implement SortedList_lookup

--- 2A/SortedList.c
+++ 2A/SortedList.c
@@ -41,19 +41,47 @@ void SortedList_insert(SortedList_t* list, SortedListElement_t* element) {
 
 
 int SortedList_delete(SortedListElement_t* element) {
+	// ensure we have a list
 	if(element == NULL) {
 		return 0; // done
 	}
 
+	// ensure element is properly part of a list before removing
 	if(element->next->prev == element->prev->next) {
 		if(opt_yield & DELETE_YIELD) {
 			sched_yield();
 		}
+			// remove
 			element->prev->next = element->next;
 			element->next->prev = element->prev;
-			return 0;
+			return 0; // success
 	}
 
-	return 1;
+	return 1; // error with the list
 }
+
+
+SortedListElement_t* SortedList_lookup(SortedList_t* list, const char* key) {
+	// ensure valid list and key
+	if(list == NULL || key == NULL) {
+		return NULL;
+	}
+
+	SortedList_t* ptr = list->next;
+
+	while(ptr != list) {
+		if(strcmp(key, ptr->key) == 0) {
+			return ptr; // found element
+		}
+
+		if(opt_yield & LOOKUP_YIELD) {
+			sched_yield();
+		}
+
+		ptr = p->next; // keep looking
+	}
+
+	return NULL; // error
+}
+
 

f234bfd29cd2336b101343f8cf3a05db63dccb96
WhiteRabbit27 <RKhillah@ucla.edu>
2018-10-26 10:32:22 -0700

    Add template dir
    Template dir has common.* which implements functions for common(ly)
    used utilitites like setting the program name, printing fatal and
    non-fatal error messages, debug messages.
    
    TODO: implement verdaic functionality to error messages.
    TODO: add @param stuff
    
    template/
    common.h and common.c - as talked about above
    
    source.c and header.c - each containing standard header files, and
    page setup.
    
    TODO: add main routine to source.c
    
    Makefile - has commonly used recipes in generic form.
    
    TODO: add test recipe and generic $(CC) for compiling recipes
    
    README - Blank readme with header file
    
    TODO: add generic descriptions of Makefile, common.h and common.c
    
    NOTE:
    Each template has desired NAME, EMAIL, ID fields. Source and header
    files each have standard preprocessor directives, like stdio, stdlib,
    etc...

--- template/common.h
+++ template/common.h
@@ -16,6 +16,27 @@ const char* program_name;
 void set_program_name(const char* argv0);
 
 
+// common exit status codes
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR 1
+
 /**
  * 
  */
+void fatal_error(const* msg);
+
+/**
+ * 
+ */
+void handle_error(char* msg);
+
+/**
+ * 
+ */
+void debug(char* msg);
+
+/**
+ *
+ */
+//TODO: Add @param's
+//TODO: Add verdaic from https://stackoverflow.com/questions/27795767/optional-arguments-in-c-function
---
+++ template/source.c
@@ -0,0 +1,13 @@
+// NAME: Richard Khillah
+// EMAIL: RKhillah@ucla.edu
+// ID: 604853262
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "common.h"

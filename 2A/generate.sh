97d65cd21134e687a5f1a93f55dac9ac3466a877
WhiteRabbit27 <RKhillah@ucla.edu>
2018-10-27 16:24:22 -0700

    add --yield to script
    also, update script to put a newline between options.
    also, add two global variables, ADDCSV and ADDMK each corresponging to
    lab2_add.csv and l2add_tests.mk. refactor code base to use ADDMK.
    TODO: refactor to use ADDCSV.
    TODO: change the name of l2add_tests.mk to lab2_add_tests.mk and
    	correspondingly update top level Makefile -include filename
    TODO: change the name of lab2_add_tests.mk from l2add_tests to
    add_tests

--- 2A/generate.sh
+++ 2A/generate.sh
@@ -8,13 +8,28 @@
 
 # if there are any previously generate makefiles or .csv files, 
 # remove them before creating the .mk recipes.
+ADDCSV="lab2_add.csv"
+#ADDMK="lab2_add.mk"
+ADDMK="l2add_tests.mk"
+
 rm -f *.mk
 rm -f lab2_add.csv
-echo "l2add_tests:" >> l2add_tests.mk
+echo "l2add_tests:" >> ${ADDMK}
 for threads in 1 2 4 8 16; do
 	for iterations in 100 1000 10000 100000; do
-		echo "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> l2add_tests.mk
+		echo "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
 		# the following declaration is for lnxsrv07
-		#echo -e "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> l2add_tests.mk
+		#echo -e "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
+	done
+done
+
+echo >> ${ADDMK}
+#echo -e "" >> ${ADDFILE}
+
+for threads in 1 2 4 8 16; do
+	for iterations in 100 1000 10000 100000; do
+		echo "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
+		# the following declaration is for lnxsrv07
+		#echo -e "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
 	done
 done

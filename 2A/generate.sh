3b0db01a749f1af9978912955158d42d6f2bb835
WhiteRabbit27 <RKhillah@ucla.edu>
2018-10-27 20:30:49 -0700

    Update generate to make appropate lab2_add-2.png test cases

--- 2A/generate.sh
+++ 2A/generate.sh
@@ -9,8 +9,8 @@
 # if there are any previously generate makefiles or .csv files, 
 # remove them before creating the .mk recipes.
 
-#ECHO="echo" # use on local machine
-ECHO="echo -e" # use on lnxsrv07
+ECHO="echo" # use on local machine
+#oECHO="echo -e" # use on lnxsrv07
 
 ADDCSV="lab2_add.csv"
 ADDMK="maketests.mk"
@@ -23,23 +23,34 @@ rm -f *.mk
 rm -f lab2_add.csv
 $ECHO "${ADDRULE}:" >> ${ADDMK}
 
-# NONE (--threads and --iterations only)
-for threads in 1 2 4 8 16; do
+## start lab2_add-1.png
+for threads in 1 2 4 8 12; do
 	for iterations in 100 1000 10000 100000; do
 		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
-		# the following declaration is for lnxsrv07
-		#$ECHO -e "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
 	done
 done
-
 $ECHO >> ${ADDMK}
-#$ECHO -e >> ${ADDFILE}
-
 # YIELD (--threads, --iterations, --yield)
 for threads in 1 2 4 8 12; do
 	for iterations in 10 20 40 80 100 1000 10000 100000; do
 		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
-		# the following declaration is for lnxsrv07
-		#$ECHO -e "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
+	done
+done
+## end lab2_add-1.png
+
+$ECHO >> ${ADDMK}
+############################################################
+$ECHO >> ${ADDMK}
+
+## start lab2_add-2.png
+for threads in 2 8; do
+	for iterations in 100 1000 10000 100000; do
+		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
+	done
+done
+$ECHO >> ${ADDMK}
+for threads in 2 8; do
+	for iterations in 100 1000 10000 100000; do
+		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
 	done
 done

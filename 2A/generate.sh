f71d13552079bc6498afc7ed7d4460a2a43e71d4
Richard Khillah <RKhillah@ucla.edu>
2018-11-01 21:37:35 -0700

    generate -2

--- 2A/generate.sh
+++ 2A/generate.sh
@@ -31,19 +31,19 @@ done
 $ECHO >> ${ADDMK}
 
 # B
-# no yield
-# for threads in 2 4 8 12; do
-# 	for iterations in 10 20 40 80 100 1000 10000 100000; do
-# 		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
-# 	done
-# done
-# # yes yield
-# for threads in 2 4 8 12; do
-# 	for iterations in 10 20 40 80 100 1000 10000 100000; do
-# 		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
-# 	done
-# done
-# $ECHO >> ${ADDMK}
+no yield
+for threads in 2 4 8 12; do
+	for iterations in 10 20 40 80 100 1000 10000 100000; do
+		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} >> lab2_add.csv" >> ${ADDMK}
+	done
+done
+# yes yield
+for threads in 2 4 8 12; do
+	for iterations in 10 20 40 80 100 1000 10000 100000; do
+		$ECHO "\t-./lab2_add --threads=${threads} --iterations=${iterations} --yield >> lab2_add.csv" >> ${ADDMK}
+	done
+done
+$ECHO >> ${ADDMK}
 
 # # C
 # # no yield

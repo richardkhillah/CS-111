b5b4f55839248b7e0f120b25cda97e5b37fff140
WhiteRabbit27 <RKhillah@ucla.edu>
2018-11-01 20:24:34 -0700

    clean up working dir

--- 2A/maketests.mk
+++
@@ -1,6 +0,0 @@
-test_add:
-	-./lab2_add --iterations=100 >> lab2_add.csv
-	-./lab2_add --iterations=1000 >> lab2_add.csv
-	-./lab2_add --iterations=10000 >> lab2_add.csv
-	-./lab2_add --iterations=100000 >> lab2_add.csv
-

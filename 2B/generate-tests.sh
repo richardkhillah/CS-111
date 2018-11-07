5296b1e1d8cbe721b8b95a3869c27d4cf8b982a5
WhiteRabbit27 <RKhillah@ucla.edu>
2018-11-06 20:01:17 -0800

    Add lab2b_list

--- 2B/generate-tests.sh
+++ 2B/generate-tests.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-LIST=- ./lab2_list
-OUT_FILE=lab2_list.csv
-
-for threads in 1 2 4 8 12 16 24; do
-	for sync_type in m s; do
-		$LIST --threads=${THREADS} --iterations=1000 --sync=${sync_type} >> $OUT_FILE
+ECHO=echo 
+LIST=./lab2_list
+OUT_FILE=lab2b_list.csv
+for sync_type in m s; do
+	for threads in 1 2 4 8 12 16 24; do
+		$LIST --threads=${threads} --iterations=1000 --sync=${sync_type} >> $OUT_FILE
 	done
 done

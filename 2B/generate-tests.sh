8bd7bcc0d63a37a592794688310994173a12c8bc
WhiteRabbit27 <RKhillah@ucla.edu>
2018-11-07 23:55:43 -0800

    shit

--- 2B/generate-tests.sh
+++ 2B/generate-tests.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-ECHO=echo
-LIST=./lab2_list
-OUT_FILE=lab2b_list.csv
+ECHO="echo"
+LIST="./lab2_list"
+OUT_FILE="lab2b_list.csv"
+TEST_FILE="tests.mk"
 for sync_type in m s; do
 	for threads in 1 2 4 8 12 16 24; do
-		$LIST --threads=${threads} --iterations=1000 --sync=${sync_type} >> $OUT_FILE
+		$ECHO "-./lab2_list --threads=${threads} --iterations=1000 --sync=${sync_type} >> lab2b_list.csv" >> ${TEST_FILE}
 	done
 done
 
 for threads in 1 4 8 12 16; do
 	for iterations in 1 2 4 8 16; do
-		$LIST --threads=${threads} --iterations=${iterations} --yield=id --lists=4 >> $OUT_FILE
+		$ECHO "-./lab2_list --threads=${threads} --iterations=${iterations} --yield=id --lists=4 >> lab2b_list.csv" >> ${TEST_FILE}
 	done
 done
 
 for sync in s m; do
 	for threads in 1 4 8 12 16; do
 		for iterations in 10 20 40 80; do
-			$LIST --threads=${threads} --iterations=${iterations} --yield=id --sync=${sync} --lists=4 >> $OUT_FILE
+			$ECHO "-./lab2_list --threads=${threads} --iterations=${iterations} --yield=id --sync=${sync} --lists=4 >> lab2b_list.csv" >> ${TEST_FILE}
 		done
 	done
 done
@@ -28,7 +29,7 @@ done
 for sync in s m; do
 	for threads in 1 2 4 8 12; do
 		for lists in 1 4 8 16; do
-			$LIST --threads=${threads} --iterations=1000 --sync=${sync} --lists=${lists} >> $OUT_FILE
+			$ECHO "-./lab2_list --threads=${threads} --iterations=1000 --sync=${sync} --lists=${lists} >> lab2b_list.csv" >> ${TEST_FILE}
 		done
 	done
 done

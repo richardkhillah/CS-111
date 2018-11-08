073f4b96a4a1ed94986f678b0d75804bbc25836b
WhiteRabbit27 <RKhillah@ucla.edu>
2018-11-07 19:57:29 -0800

    add test cases to generate-tests

--- 2B/generate-tests.sh
+++ 2B/generate-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ECHO=echo 
+ECHO=echo
 LIST=./lab2_list
 OUT_FILE=lab2b_list.csv
 for sync_type in m s; do
@@ -8,3 +8,18 @@ for sync_type in m s; do
 		$LIST --threads=${threads} --iterations=1000 --sync=${sync_type} >> $OUT_FILE
 	done
 done
+
+for threads in 1 4 8 12 16; do
+	for iterations in 1 2 4 8 16; do
+		$LIST ==threads=${threads} --iterations=${iterations} --yield=id --lists=4 >> $OUT_FILE
+	done
+done
+
+for sync in s m; do
+	for threads in 1 4 8 12 16; do
+		for iterations in 10 20 40 80; do
+			$LIST ==threads=${threads} --iterations=${iterations} --yield=id --sync=${sync} --lists=4 >> $OUT_FILE
+		done
+	done
+done
+

3f7931663e141f4fe071c5bfa87825803961234d
WhiteRabbit27 <RKhillah@ucla.edu>
2018-12-03 15:07:15 -0800

    a couple more changes. Test on beaglebone needed

--- 4C/my_check.sh
+++ 4C/my_check.sh
@@ -3,7 +3,6 @@
 # ID: 604853262
 
 #!/bin/bash
-#UID="604853262"
 TCP_PGM="lab4c_tcp"
 TLS_PGM="lab4c_tls"
 TARBALL="$PGM-604853262.tar.gz"
@@ -16,8 +15,8 @@ LOG=log.txt
 
 # begin testing
 echo Running tests...
-#echo ./lab4c_tcp --id=369713302 --host=lever.cs.ucla.edu 18000 --log=tcp_log.txt
-#./$TCP_PGM --id=$FAKEID --host=$HOST $TCP_PORT --log=$LOG
+echo ./lab4c_tcp --id=369713302 --host=lever.cs.ucla.edu 18000 --log=tcp_log.txt
+./$TCP_PGM --id=$FAKEID --host=$HOST $TCP_PORT --log=$LOG
 echo ./lab4c_tls --id=369713302 --host=lever.cs.ucla.edu 19000 --log=tls_log.txt
 ./$TLS_PGM --id=$FAKEID --host=$HOST $TLS_PORT --log=$LOG
 
